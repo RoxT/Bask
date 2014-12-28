1aed07f74b75bfa0dd14828dee56650e7f9fdb0f
Roxanne Taylor <roxanne.e.taylor@gmail.com>
2014-12-27 19:50:26 -0700

    Added database. Only Title and Text in schema

--- bask.py
+++ bask.py
@@ -1,11 +1,107 @@
-from flask import Flask, request, session, g, redirect, url_for, \
-	abort, render_template, flash
+# imports
+import sqlite3
+from flask import Flask, render_template, request, session, g, redirect, url_for, \
+    abort, render_template, flash
+from contextlib import closing
+
+# configuration
+DATABASE = '/bask.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+# create our little application :)
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+# DATABASE OPERATIONS
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+
+# ROUTES
 
 @app.route('/')
 def hello_world():
     return render_template('home.html')
 
+
+@app.route('/about')
+def about():
+    return render_template('layout.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('layout.html')
+
+
+# login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('Welcome back', app.config['USERNAME'])
+            return redirect(url_for('band'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('band'))
+
+
+# Band entries
+
+@app.route('/band')
+def band():
+    cursor = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cursor.fetchall()]
+    return render_template('band.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('band'))
+
+# MAIN
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
+
